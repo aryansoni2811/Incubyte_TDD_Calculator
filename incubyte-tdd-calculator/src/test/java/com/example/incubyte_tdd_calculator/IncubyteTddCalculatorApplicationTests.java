0c6d0d94929b789ea734e852702c92ff83213ff7
aryansoni2811 <onlyaryan2003@gmail.com>
2025-07-04 22:54:05 +0530

    Add test for multiple numbers - already passes

--- incubyte-tdd-calculator/src/test/java/com/example/incubyte_tdd_calculator/IncubyteTddCalculatorApplicationTests.java
+++ incubyte-tdd-calculator/src/test/java/com/example/incubyte_tdd_calculator/IncubyteTddCalculatorApplicationTests.java
@@ -29,6 +29,14 @@ class IncubyteTddCalculatorApplicationTests {
 		assertEquals(10, calculator.add("4,6"));
 	}
 
+	@Test
+	public void should_handle_multiple_numbers() {
+		StringCalculator calculator = new StringCalculator();
+		assertEquals(15, calculator.add("1,2,3,4,5"));
+		assertEquals(21, calculator.add("1,2,3,4,5,6"));
+	}
+
+
 
 
 }
