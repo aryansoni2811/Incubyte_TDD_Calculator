e12c807c4df9f227b4561a2ddfa9bb0bb567471d
aryansoni2811 <onlyaryan2003@gmail.com>
2025-07-04 22:35:37 +0530

    Add failing test for single number

--- incubyte-tdd-calculator/src/test/java/com/example/incubyte_tdd_calculator/IncubyteTddCalculatorApplicationTests.java
+++ incubyte-tdd-calculator/src/test/java/com/example/incubyte_tdd_calculator/IncubyteTddCalculatorApplicationTests.java
@@ -15,5 +15,13 @@ class IncubyteTddCalculatorApplicationTests {
 		assertEquals(0, calculator.add(""));
 	}
 
+	@Test
+	public void should_return_number_for_single_number() {
+		StringCalculator calculator = new StringCalculator();
+		assertEquals(1, calculator.add("1"));
+		assertEquals(5, calculator.add("5"));
+	}
+
+
 
 }
